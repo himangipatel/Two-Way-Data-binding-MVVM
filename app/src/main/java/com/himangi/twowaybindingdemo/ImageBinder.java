a0583816a0fb71284bce0dc89f8fb671114ccf98
Himangi Patel <himangi.1411@gmail.com>
2016-10-15 17:58:24 +0530

    load image using picasso (Web URL)

--- app/src/main/java/com/himangi/twowaybindingdemo/ImageBinder.java
+++ app/src/main/java/com/himangi/twowaybindingdemo/ImageBinder.java
@@ -1,5 +1,6 @@
 package com.himangi.twowaybindingdemo;
 
+import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
@@ -14,13 +15,25 @@ public class ImageBinder {
         //NO-OP
     }
 
+    //user this as a attribute in xml file like app:imageUrl="@{dataname.url}"
     @BindingAdapter("imageUrl")
     public static void setImageUrl(ImageView imageView, Drawable url) {
         //Context context = imageView.getContext();
 
         imageView.setImageDrawable(url);
 
-        //if u want to load web URL then use below code
+    }
+
+
+    //if u want to load web URL then use below code
+    @BindingAdapter("imageUrl")
+    public static void setImageUrl(ImageView imageView, String url) {
+        Context context = imageView.getContext();
+
+        if (url!=null){
+
+
+            //if u want to load web URL then use below code
 //        Picasso picasso =  Picasso.with(view.getContext());
 //        picasso.setIndicatorsEnabled(true);
 //        picasso.load(url)
@@ -28,6 +41,8 @@ public class ImageBinder {
 //                .placeholder(R.drawable.ic_img_placeholder)
 //                .into(view);
 
+        }
     }
+
 
 }
